0866a1ced665f0464d129c8a2c3245650d8bc7f3
luckyQing <1634753825@qq.com>
2022-11-28 11:06:40 +0800

    fixbug: 修复bigint值为base64问题

--- src/test/java/com/collin/learning/flink/test/FlinkTest.java
+++ src/test/java/com/collin/learning/flink/test/FlinkTest.java
@@ -7,10 +7,15 @@ import com.ververica.cdc.debezium.JsonDebeziumDeserializationSchema;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.connector.sink2.Sink;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.kafka.connect.json.DecimalFormat;
+import org.apache.kafka.connect.json.JsonConverterConfig;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -18,22 +23,25 @@ public class FlinkTest {
 
     @Test
     void test() throws Exception {
+        Map<String, Object> customConverterConfigs = new HashMap<>();
+        customConverterConfigs.put(JsonConverterConfig.DECIMAL_FORMAT_CONFIG, DecimalFormat.NUMERIC.name());
+
         MySqlSource<String> mySqlSource = MySqlSource.<String>builder()
                 .hostname(MysqlConfig.HOST)
                 .port(MysqlConfig.PORT)
-                .databaseList(MysqlConfig.DATABASE_LIST) // set captured database
-                .tableList(MysqlConfig.TABLE_LIST) // set captured table
+                .databaseList(MysqlConfig.DATABASE_LIST)
+                .tableList(MysqlConfig.TABLE_LIST)
                 .username(MysqlConfig.USERNAME)
                 .password(MysqlConfig.PASSWORD)
-                .deserializer(new JsonDebeziumDeserializationSchema()) // converts SourceRecord to JSON String
+                .deserializer(new JsonDebeziumDeserializationSchema(false, customConverterConfigs))
                 .build();
 
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-        // enable checkpoint
         env.enableCheckpointing(3000)
                 .fromSource(mySqlSource, WatermarkStrategy.noWatermarks(), "MySQL Source")
-                .setParallelism(MysqlConfig.TABLE_LIST.length).sinkTo((Sink<String>) context -> new RedisSinkWriter());
+                .setParallelism(MysqlConfig.TABLE_LIST.length)
+                .sinkTo((Sink<String>) context -> new RedisSinkWriter());
 
 
         env.execute("Print MySQL Snapshot + Binlog");
